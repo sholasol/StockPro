ff68534e930bc0794ea89b8ad984a640a660f057
Solomon <sholasolomon@yahoo.com>
2024-01-11 23:13:29 +0000

    Data, Model and Dtos

---
+++ api/api/Mappers/StockMapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using api.Dtos.Stock;
+using api.Models;
+
+namespace api.Mappers
+{
+	public static class StockMapper
+	{
+		public static StockDto ToStockDto(this Stock StockModel)
+		{
+			return new StockDto
+			{
+				Id = StockModel.Id,
+				Symbol = StockModel.Symbol,
+				CompanyName = StockModel.CompanyName,
+				Purchase = StockModel.Purchase,
+				LastDiv =  StockModel.LastDiv,
+				Industry	= StockModel.Industry,
+				MarketCap = StockModel.MarketCap
+			};
+		}
+	}
+}
+
